13ab26cf1b7ba48b039817382ceb54f015aedaca
peyas <peyas3500@gmail.com>
2022-02-25 19:41:59 +0600

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Navbar class="mb-3"></Navbar>
+    <div class="container">
+
+    <Home></Home>
+    </div>
+  </div>
+</template>
+<script>
+import Home from "./View/Home.vue";
+import Navbar from "./components/Nabvar.vue";
+
+export default {
+  name: "App",
+  components: {
+    Home,
+    Navbar,
+  },
+};
+</script>
---
+++ src/View/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <WeatherCard></WeatherCard>
+  </div>
+</template>
+
+<script>
+import WeatherCard from "../components/WeatherCard.vue";
+export default {
+  data() {
+    return {
+      wweather: [],
+    };
+  },
+  components: {
+    WeatherCard,
+  },
+  mounted(){
+      this.getWeatherData();
+  },
+  methods: {
+    getWeatherData() {
+      fetch("http://https://jsonplaceholder.typicode.com/users")
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    },
+  },
+};
+</script>
